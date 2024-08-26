4856ad97ad7d0cc72398220fb8c4b71c07ed466f
OmarSO08 <omar.souabni@emory.edu>
2024-08-26 17:24:46 -0400

    update 3

--- R code Final Project.R
+++ R code Final Project.R
@@ -2,8 +2,9 @@
 load("data/shooting_data.rda")
 df<- shooting_data
 
-library(gtsummary)
+
 #create a table of dicriptive statistics
+library(gtsummary)
 tbl_summary(
 	data = df,
 	by = State,
@@ -15,15 +16,37 @@ tbl_summary(
 	missing_text = "Missing"
 )
 
+
+
 # Fit a linear regression model
 model <- lm(`Killed (includes shooter)` ~ `Number of Shots Fired`, data = df)
 
-library(ggplot2)
 
-# Histogram of 'Killed (includes shooter)' using ggplot2
+
+# Create a figure: Histogram of 'Killed (includes shooter)'
+library(ggplot2)
 ggplot(df, aes(x = `Killed (includes shooter)`)) +
 	geom_histogram(binwidth = 1, fill = "skyblue", color = "black") +
 	labs(title = "Histogram of Killed (includes shooter)",
 			 x = "Number of People Killed (including shooter)",
 			 y = "Frequency") +
 	theme_minimal()
+
+
+
+# Write a function to calculate standard deviation of 'Killed (includes shooter)' for each state
+library(dplyr)
+calculate_sd_per_state <- function(data) {
+
+	result <- data %>%
+		group_by(State) %>%
+		summarise(SD_Killed = sd(`Killed (includes shooter)`, na.rm = TRUE))
+
+
+	return(result)
+}
+# Use the function to calculate standard deviation of 'Killed (includes shooter)' for each state
+sd_per_state <- calculate_sd_per_state(df)
+
+# Print the standard deviations
+print(sd_per_state)
